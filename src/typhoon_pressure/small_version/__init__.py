e963350f8c3aec5822ac35a13342d38a8f112137
nayehyeon61-glitch <nayehyeon61@gmail.com>
2026-09-03 19:16:13 +0900

    Export probabilistic survival and ensemble utilities

--- src/typhoon_pressure/small_version/__init__.py
+++ src/typhoon_pressure/small_version/__init__.py
@@ -1,38 +1,26 @@
-"""Small dual-objective model for long-range distribution and local track learning."""
+"""Small probabilistic model for long-range distribution, survival and local track learning."""
 
-from .config import (
-    DualLossConfig,
-    EastAsiaBounds,
-    SmallModelConfig,
-    TransformerConfig,
-    WeatherNextTokenConfig,
-)
+from .config import DualLossConfig, EastAsiaBounds, SmallModelConfig, TransformerConfig, WeatherNextTokenConfig
 from .dataset import DualTargetDataset, SpatialDistributionLookup, WeatherNextDualTargetDataset
 from .losses import DualObjectiveLoss
 from .model import SmallDualScaleModel, WeatherNextFusionTransformer
+from .probabilistic import (
+    ProbabilisticSamples,
+    conditional_distribution,
+    hazard_logits_to_survival,
+    joint_distribution,
+    sample_survival_locations,
+)
 from .weathernext_bridge import (
-    DirectoryForecastTokenStore,
-    ForecastTokens,
-    WeatherNextForecastTokenizer,
-    run_and_save_weathernext_tokens,
-    save_forecast_tokens,
+    DirectoryForecastTokenStore, ForecastTokens, WeatherNextForecastTokenizer,
+    run_and_save_weathernext_tokens, save_forecast_tokens,
 )
 
 __all__ = [
-    "DualLossConfig",
-    "DualObjectiveLoss",
-    "DualTargetDataset",
-    "DirectoryForecastTokenStore",
-    "EastAsiaBounds",
-    "ForecastTokens",
-    "SmallDualScaleModel",
-    "SmallModelConfig",
-    "SpatialDistributionLookup",
-    "TransformerConfig",
-    "WeatherNextDualTargetDataset",
-    "WeatherNextForecastTokenizer",
-    "WeatherNextFusionTransformer",
-    "WeatherNextTokenConfig",
-    "run_and_save_weathernext_tokens",
-    "save_forecast_tokens",
+    "DualLossConfig", "DualObjectiveLoss", "DualTargetDataset", "DirectoryForecastTokenStore",
+    "EastAsiaBounds", "ForecastTokens", "ProbabilisticSamples", "SmallDualScaleModel",
+    "SmallModelConfig", "SpatialDistributionLookup", "TransformerConfig", "WeatherNextDualTargetDataset",
+    "WeatherNextForecastTokenizer", "WeatherNextFusionTransformer", "WeatherNextTokenConfig",
+    "conditional_distribution", "hazard_logits_to_survival", "joint_distribution",
+    "run_and_save_weathernext_tokens", "sample_survival_locations", "save_forecast_tokens",
 ]
